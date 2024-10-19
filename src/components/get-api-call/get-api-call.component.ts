e075d9cd42023bac16e90adfd7e856f1fc57a841
nikhil <001nikhilshirsath001@gmail.com>
2024-10-19 22:23:01 +0530

    reactive form

---
+++ src/components/get-api-call/get-api-call.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-get-api-call',
+  standalone: true,
+  imports: [],
+  templateUrl: './get-api-call.component.html',
+  styleUrl: './get-api-call.component.css'
+})
+export class GetApiCallComponent {
+
+}
--- src/components/reactive-form/reactive-form.component.ts
+++ src/components/reactive-form/reactive-form.component.ts
@@ -1,12 +1,30 @@
+import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,JsonPipe],
   templateUrl: './reactive-form.component.html',
   styleUrl: './reactive-form.component.css'
 })
 export class ReactiveFormComponent {
 
+  employee:FormGroup = new FormGroup({
+    firstName :  new FormControl('',[Validators.required,Validators.minLength(3),Validators.maxLength(30)]),
+    lastName:new FormControl('',[Validators.required,Validators.minLength(3),Validators.maxLength(30)]),
+    userName :  new FormControl('',[Validators.required,Validators.minLength(3),Validators.email]),
+    state:new FormControl(),
+    city :  new FormControl(),
+    zipCode:new FormControl(),
+    isAcceptTerms: new FormControl(false,[Validators.required])
+  
+  });
+
+  formValue:any;
+
+  onSubmit(){
+    this.formValue= this.employee.value;
+  }
 }
